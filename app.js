26bc810513b76ea8ebcea13b6f0040e7446900f3
Stephanie Barker <song_mage@yahoo.com>
2017-08-25 11:29:36 -0400

    logging in with passport works

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 const session = require("express-session");
 const { User, List, ListItem } = require("./models");
 
+// Basic middleware
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const hbs = exphbs.create({
@@ -22,13 +24,65 @@ app.use(
 	})
 );
 
-app.get("/", (req, res) => {
-	User.findById(1, {
-		include: [{ model: List, include: [{ model: ListItem }] }]
-	}).then(user => {
-		res.send(`<pre>${JSON.stringify(user, null, 2)}</pre>`);
+// Passport
+
+const passport = require("passport");
+app.use(passport.initialize());
+app.use(passport.session());
+
+const LocalStrategy = require("passport-local").Strategy;
+
+passport.use(
+	new LocalStrategy(function(username, password, done) {
+		User.find({
+			where: { username: username }
+		}).then(user => {
+			console.log(user);
+			if (!user || !user.validatePassword(password)) {
+				return done(null, false);
+			}
+			return done(null, user);
+		});
+	})
+);
+
+passport.serializeUser(function(user, done) {
+	done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+	User.findById(id).then(user => {
+		done(null, user);
 	});
 });
+
+//routes
+
+app.get("/", (req, res) => {
+	if (req.user) {
+		User.findById(req.user.id, {
+			include: [{ model: List, include: [{ model: ListItem }] }]
+		}).then(user => {
+			user = JSON.stringify(user, null, 2);
+			res.render("dashboard", { user });
+		});
+	} else {
+		res.render("login");
+	}
+});
+
+app.get("/login", (req, res) => {
+	res.render("login");
+});
+
+app.post(
+	"/login",
+	passport.authenticate("local", {
+		successRedirect: "/",
+		failureRedirect: "/login",
+		failureFlash: true
+	})
+);
 
 app.listen(3000, () => {
 	console.log("Now listening...");
